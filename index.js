195c8b35c915b634a33aff1097d302bc94e54a42
Moeez Ayub <164150984+moeezayyub@users.noreply.github.com>
2026-08-20 11:51:17 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js");
+}
+
+let deferredPrompt;
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+
+  deferredPrompt = e;
+
+  installBtn.hidden = false;
+});
+
+installBtn.addEventListener("click", async () => {
+  if (!deferredPrompt) return;
+
+  deferredPrompt.prompt();
+
+  const result = await deferredPrompt.userChoice;
+
+  console.log(result.outcome);
+
+  deferredPrompt = null;
+
+  installBtn.hidden = true;
+});
